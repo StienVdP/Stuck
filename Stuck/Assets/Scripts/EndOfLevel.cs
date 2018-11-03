af73bf61b8b91b507b380833c377046291e6f265
Justine <almes.justine@gmail.com>
2018-11-02 23:12:07 -0400

    update player mouvement

---
+++ Stuck/Assets/Scripts/EndOfLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndOfLevel : MonoBehaviour {
+
+    public GameObject exitDoor;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Start exit door");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            Debug.Log("OnCollisionEnter2D with : " + col.name);
+            // Animation ouverture de porte
+            // Changement de scène
+        }
+    }
+}
---
+++ Stuck/Assets/Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformerController : PhysicsObject
+{
+
+    public float maxSpeed = 100;
+    public float jumpTakeOffSpeed = 100;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    // Use this for initialization
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded) // Grounded : pour sauter il faut que le player soit au sol => pas de double saut
+        {
+            velocity.y = jumpTakeOffSpeed; // Fait monter = saut
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f; // Fait re dessandre = fin du saut
+            }
+        }
+
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f)); // Flip le sprit quand on va dans l'autre direction
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        animator.SetBool("Ground", grounded);
+        animator.SetFloat("Speed", Mathf.Abs(velocity.x) / maxSpeed);
+        Debug.Log("Ground : " + grounded);
+
+        targetVelocity = move * maxSpeed; // Fait avancer
+    }
+}
