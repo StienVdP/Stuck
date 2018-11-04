96158ae47a63edaa73e297f9d28e09dafa7262d7
Justine <almes.justine@gmail.com>
2018-11-04 14:44:55 -0500

    update player

--- Stuck/Assets/Scripts/EndOfLevel.cs
+++ Stuck/Assets/Scripts/EndOfLevel.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndOfLevel : MonoBehaviour {
 
     public GameObject exitDoor;
+    private Animator animation;
+    public string nextLevelName;
 
     // Use this for initialization
     void Start () {
         Debug.Log("Start exit door");
+        animation = exitDoor.GetComponent<Animator>();
+        animation.enabled = false;
     }
 	
 	// Update is called once per frame
@@ -22,7 +27,14 @@ public class EndOfLevel : MonoBehaviour {
         {
             Debug.Log("OnCollisionEnter2D with : " + col.name);
             // Animation ouverture de porte
+            animation.enabled = true;
             // Changement de scène
+            StartCoroutine(changeScene(nextLevelName));
         }
+    }
+
+    IEnumerator changeScene(string levelName) {
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(levelName);
     }
 }
