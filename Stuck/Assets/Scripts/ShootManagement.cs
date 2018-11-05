d8a83f838189aec5b5bc065bfc22a8e6d29e705b
Justine <almes.justine@gmail.com>
2018-11-05 15:41:23 -0500

    fix animation player + bullet

--- Stuck/Assets/Scripts/ShootManagement.cs
+++ Stuck/Assets/Scripts/ShootManagement.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShootManagement : MonoBehaviour {
-    
+    private Vector3 speed = new Vector3(0.1f, 0.0f, 0.0f);
+    private float timeLeft;
 
-	// Use this for initialization
-	void Start () {
-        StartCoroutine("mouvement");
+    // Use this for initialization
+    void Start ()
+    {
+        timeLeft = 2.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//Verifie s'il y a collision et quand ya collision on fait ce qu'il faut faire suivant l'objet collider7
-        // Destroy quand il y a collision ou au bou de x seconde ou x distance
-	}
+        this.transform.position += speed; // Mouvement pour le moment que vers la droite
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0) // Après X seconde elle est détruite
+        {
+            Destroy(gameObject);
+        }
+        // Il faudrait faire en fonction de la distance ou faire disparaitre dès qu'elle sort de l'écran
+    }
 
-    public IEnumerator creationOfEffectZone()
+    void OnTriggerEnter2D()
     {
-
-        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
     }
 }
