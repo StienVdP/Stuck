d7ff7545d64dd64efccabb6360987159c1e4fac6
ThomasLev <thomas.lev@orange.fr>
2018-11-11 20:03:20 -0500

    Plateformes + double saut + mouvements plus fluides

---
+++ Stuck/Assets/Scripts/PitBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitBehaviour : MonoBehaviour {
+
+    private GameObject gameManager;
+    private GameManager gameManagerScript;
+	// Use this for initialization
+	void Awake () {
+        gameManager = GameObject.Find("GameManager");
+        gameManagerScript = gameManager.GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+			gameManagerScript.setLife(0);
+        }
+
+    }
+
+}
