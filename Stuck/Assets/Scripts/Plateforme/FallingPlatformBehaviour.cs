ee3e02618fa2421e2f4d1344b27d60d36203ebec
ThomasLev <thomas.lev@orange.fr>
2018-12-09 17:10:42 -0500

    fix platform

--- Stuck/Assets/Scripts/Plateforme/FallingPlatformBehaviour.cs
+++ Stuck/Assets/Scripts/Plateforme/FallingPlatformBehaviour.cs
@@ -9,17 +9,21 @@ public class FallingPlatformBehaviour : MonoBehaviour {
 	public float fallDelay;
 	public float spawnDelay;
 	private Vector3 initialPos;
+	private bool isFalling;
+
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D>();
 		rb2d.isKinematic = true;
 		boxCollider = GetComponent<BoxCollider2D>();
 		initialPos = transform.position;
+		isFalling = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (!isFalling)
+			transform.position = initialPos;
 	}
 
 	private void OnCollisionEnter2D(Collision2D col){
@@ -31,6 +35,7 @@ public class FallingPlatformBehaviour : MonoBehaviour {
 
 	IEnumerator fall(){
 		yield return new WaitForSeconds(fallDelay);
+		isFalling = true;
 		rb2d.isKinematic = false;
 		GetComponent<Collider2D>().isTrigger = true;
 		yield return 0;
@@ -39,6 +44,7 @@ public class FallingPlatformBehaviour : MonoBehaviour {
 	IEnumerator respawn()
     {
         yield return new WaitForSeconds(spawnDelay);
+		isFalling = false;
         rb2d.isKinematic = true;
         boxCollider.isTrigger = false;
         transform.position = initialPos;
