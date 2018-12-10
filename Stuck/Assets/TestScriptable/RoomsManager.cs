f2f63bfcf6c719226bb5f1776c46f2a7ea99132a
Justine <vdpoelstien@gmail.com>
2018-12-10 16:20:11 -0500

    supprimer mes tests

--- Stuck/Assets/TestScriptable/RoomsManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "DataRooms", menuName = "ScriptableObjects/RoomsManager", order = 1)]
-public class RoomsManager : ScriptableObject
-{
-    public RoomManagerScriptableObject[] dataFiles;
-    
-}
